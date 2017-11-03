add91eeaf1e233bef8f82dd18b2bd5ec76a3ddb1
Michael Roy <Mike@michaels-imac.local>
2017-11-03 12:37:42 -0500

    Initial Commit

---
+++ prog3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prog3
+//
+//  Created by Michael Cole on 11/3/17.
+//  Copyright © 2017 Michael Cole. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
