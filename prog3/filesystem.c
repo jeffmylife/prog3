72ffd95c27440d12bc9a113da3ef99cacc158c85
Michael Roy <Mike@michaels-imac.local>
2017-11-03 12:51:42 -0500

    initial commit

---
+++ prog3/filesystem.c
@@ -0,0 +1,59 @@
+//
+//  filesystem.c
+//  prog3
+//
+//  Created by Michael Cole on 11/3/17.
+//  Copyright © 2017 Michael Cole. All rights reserved.
+//
+
+#include "filesystem.h"
+
+// function prototypes for filesystem API
+
+// open existing file with pathname 'name' and access mode 'mode'.  Current file
+// position is set at byte 0.  Returns NULL on error. Always sets 'fserror' global.
+File open_file(char *name, FileMode mode);
+
+// create and open new file with pathname 'name' and access mode 'mode'.  Current file
+// position is set at byte 0.  Returns NULL on error. Always sets 'fserror' global.
+File create_file(char *name, FileMode mode);
+
+// close 'file'.  Always sets 'fserror' global.
+void close_file(File file);
+
+// read at most 'numbytes' of data from 'file' into 'buf', starting at the
+// current file position.  Returns the number of bytes read. If end of file is reached,
+// then a return value less than 'numbytes' signals this condition. Always sets
+// 'fserror' global.
+unsigned long read_file(File file, void *buf, unsigned long numbytes);
+
+// write 'numbytes' of data from 'buf' into 'file' at the current file position.
+// Returns the number of bytes written. On an out of space error, the return value may be
+// less than 'numbytes'.  Always sets 'fserror' global.
+unsigned long write_file(File file, void *buf, unsigned long numbytes);
+
+// sets current position in file to 'bytepos', always relative to the beginning of file.
+// Seeks past the current end of file should extend the file. Always sets 'fserror'
+// global.
+void seek_file(File file, unsigned long bytepos);
+
+// returns the current length of the file in bytes. Always sets 'fserror' global.
+unsigned long file_length(File file);
+
+// deletes the file named 'name', if it exists and if the file is closed.
+// Fails if the file is currently open. Returns 1 on success, 0 on failure.
+// Always sets 'fserror' global.
+int delete_file(char *name);
+
+// determines if a file with 'name' exists and returns 1 if it exists, otherwise 0.
+// Always sets 'fserror' global.
+int file_exists(char *name);
+
+// describe current filesystem error code by printing a descriptive message to standard
+// error.
+void fs_print_error(void);
+
+// filesystem error code set (set by each filesystem function)
+extern FSError fserror;
+
+}
